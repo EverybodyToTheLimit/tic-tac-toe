fe6b84663d635c43fbe8107c0337d698f6201be1
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-11-29 17:01:16 +0000

    set up basic structie, add the gameBoard IIFE @

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+var gameBoard = (function() {
+
+        var fields = [{a1: ""}, {a2: ""}, {a3: ""}, {b1: ""}, {b2: ""}, {b3: ""}, {c1: ""}, {c2: ""}, {c3: ""} ];
+        var players = ["Jack", "Jill"];
+        var gamePlay = () => {
+    
+        };
+        
+        return {
+            fields, players
+        }
+    console.log(gameBoard.fields);
+})();
