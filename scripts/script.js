7f2b960a4c6c3783c7562c15e3c4883209162c0d
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-11-30 14:32:05 +0000

    add update field functionality and expose it to rest of program

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,44 @@
 var gameBoard = (function() {
 
-        var fields = [{a1: ""}, {a2: ""}, {a3: ""}, {b1: ""}, {b2: ""}, {b3: ""}, {c1: ""}, {c2: ""}, {c3: ""} ];
-        var players = ["Jack", "Jill"];
-        var gamePlay = () => {
-    
+        var fields = [{fieldName: "a1", value: "x"}, {fieldName: "a2", value: ""}, {fieldName: "a3", value: ""}, {fieldName: "b1", value: ""}, {fieldName: "b2", value: ""}, {fieldName: "b3", value: "o"}, {fieldName: "c1", value: ""}, {fieldName: "c2", value: ""}, {fieldName: "c3", value: ""} ];
+        var players = [{name: "Jack", symbol: "x"},{name: "Jill", symbol : "o"}];
+        var drawBoard = () => {
+           console.log("cool") 
         };
+
+
+        var currentTurn = "Jack"
+
+        var updateField = (field, player) => {
+                let tempResult = false;
+                let playerSymbol = "";
+                players.find(function(user, index2){
+                    if(user.name == player) {
+                        playerSymbol = user.symbol;
+                    }
+                })
+                fields.find(function(post, index) {
+                if(post.fieldName == field) {
+                    if(post.value != "o" && post.value != "x") {
+                        tempResult = true;
+                        post.value = playerSymbol;
+                        alert("valid");
+                        return true;
+                    }
+                    else {
+
+                        tempResult = false;
+                        alert("not valid");
+                        return false;
+                        } 
+                    
+                }})
+            return tempResult;
+            
+        }
         
         return {
-            fields, players
+            updateField
         }
-    console.log(gameBoard.fields);
 })();
+
